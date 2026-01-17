940d6a5a0f4d55085ded50be9d56777d6f570478
KenWilliamson <kenulborapublic@gmail.com>
2026-01-17 15:02:58 -0500

    goroutines

--- server.go
+++ server.go
@@ -3,9 +3,35 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
+	"time"
 )
 
 func main() {
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for i := 1; i <= 10; i++ {
+			fmt.Println("goroutine 1 = ", i)
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		duration := time.Second
+		time.Sleep(duration * 3)
+		defer wg.Done()
+
+		for i := 11; i <= 20; i++ {
+			fmt.Println("goroutine 2 = ", i)
+		}
+	}()
+
+	wg.Wait()
+
 	port := "3000"
 	msg := "Server starting on port "
 	fmt.Println(msg + port)
